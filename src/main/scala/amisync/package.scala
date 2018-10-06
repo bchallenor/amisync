7b78938fbbc6fbf7ba6a7ffaec17d9ffb3e0bfd3
Ben Challenor <ben@challenor.org>
2018-10-06 14:12:52 +0100

    Check for null when constructing typed wrappers

--- src/main/scala/amisync/package.scala
+++ src/main/scala/amisync/package.scala
@@ -1,17 +1,44 @@
 package object amisync {
-  case class RegionName(name: String) extends AnyVal
-  case class RoleName(name: String) extends AnyVal
-  case class Bucket(name: String) extends AnyVal
-  case class Key(name: String) extends AnyVal
-  case class KeyPrefix(name: String) extends AnyVal
-  case class AmiId(id: String) extends AnyVal
-  case class AmiName(name: String) extends AnyVal
+  case class RegionName(name: String) {
+    require(name ne null)
+  }
+
+  case class RoleName(name: String) {
+    require(name ne null)
+  }
+
+  case class Bucket(name: String) {
+    require(name ne null)
+  }
+
+  case class Key(name: String) {
+    require(name ne null)
+  }
+
+  case class KeyPrefix(name: String) {
+    require(name ne null)
+  }
+
+  case class AmiId(id: String) {
+    require(id ne null)
+  }
+
+  case class AmiName(name: String) {
+    require(name ne null)
+  }
+
   object AmiName {
     def deriveFrom(key: Key): AmiName = {
       // Strip directory name and extension
       AmiName(key.name.split("/").last.split("\\.").head)
     }
   }
-  case class ImportTaskId(id: String) extends AnyVal
-  case class SnapshotId(id: String) extends AnyVal
+
+  case class ImportTaskId(id: String) {
+    require(id ne null)
+  }
+
+  case class SnapshotId(id: String) {
+    require(id ne null)
+  }
 }
