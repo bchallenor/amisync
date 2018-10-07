9b32f1437d912b04caf89400d9c35646b5a2e6f7
Ben Challenor <ben@challenor.org>
2018-10-07 11:44:10 +0100

    Rewrite using ImportSnapshotTask and CopySnapshotTask

--- src/main/scala/amisync/ImportAmiFromS3Task.scala
+++ src/main/scala/amisync/ImportAmiFromS3Task.scala
@@ -1,34 +1,16 @@
 package amisync
 
-import com.amazonaws.services.ec2.model.{ImportSnapshotRequest, SnapshotDiskContainer, UserBucket}
-
 case class ImportAmiFromS3Task(amiName: AmiName, bucket: Bucket, key: Key) extends Task {
   override def run(config: Config): Set[Task] = {
-    import config._
-    val res = ec2.importSnapshot({
-      val req = new ImportSnapshotRequest()
-      req.setRoleName(vmImportRoleName.name)
-      req.setDiskContainer({
-        val diskContainer = new SnapshotDiskContainer
-        diskContainer.setDescription(amiName.name)
-        diskContainer.setFormat("RAW")
-        diskContainer.setUserBucket({
-          val userBucket = new UserBucket()
-          userBucket.setS3Bucket(bucket.name)
-          userBucket.setS3Key(key.name)
-          userBucket
-        })
-        diskContainer
-      })
-      req
-    })
+    val description = amiName.name
+    val encrypt = true
     Set(
-      WaitForImportSnapshotTask(
-        ImportTaskId(res.getImportTaskId),
-        snapshotId => Set(
-          ImportAmiFromSnapshotTask(amiName, snapshotId)
-        )
-      )
+      ImportSnapshotTask(bucket, key, importedSnapshotId => Set(
+        CopySnapshotTask(importedSnapshotId, description, encrypt, encryptedSnapshotId => Set(
+          DeleteSnapshotTask(importedSnapshotId),
+          RegisterAmiTask(amiName, encryptedSnapshotId)
+        ))
+      ))
     )
   }
 }
