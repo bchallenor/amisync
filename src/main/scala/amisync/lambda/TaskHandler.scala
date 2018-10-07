85524f05f0cb77903df70d3f229e0b1a48a925a7
Ben Challenor <ben@challenor.org>
2018-10-07 18:36:24 +0100

    SQS sends an SQSEvent, not the message directly

--- src/main/scala/amisync/lambda/TaskHandler.scala
+++ src/main/scala/amisync/lambda/TaskHandler.scala
@@ -3,11 +3,19 @@ package amisync.lambda
 import amisync._
 import amisync.json._
 import com.amazonaws.services.lambda.runtime._
+import com.amazonaws.services.lambda.runtime.events.SQSEvent
 import com.amazonaws.services.sqs._
 import spray.json._
 
-class TaskHandler extends RequestJsonHandler[Task, Unit] {
-  override def handleRequest(task: Task, context: Context): Unit = {
+class TaskHandler extends RequestHandler[SQSEvent, Unit] {
+  override def handleRequest(event: SQSEvent, context: Context): Unit = {
+    require(event.getRecords.size == 1)
+    val taskStr = event.getRecords.get(0).getBody
+    val task = try JsonParser(taskStr).convertTo[Task] catch {
+      case e: Exception =>
+        throw new IllegalArgumentException(s"Could not parse JSON: $taskStr", e)
+    }
+
     println(s"Running task: ${task.toJson.compactPrint}")
 
     val config = Config.default
