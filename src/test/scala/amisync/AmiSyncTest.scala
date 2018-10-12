6f7ca8a76ec1ead5f2740473a38c6ecfe1ac76bd
Ben Challenor <ben@challenor.org>
2018-10-12 22:52:11 +0100

    Update test to reflect new S3 key format

--- src/test/scala/amisync/AmiSyncTest.scala
+++ src/test/scala/amisync/AmiSyncTest.scala
@@ -11,7 +11,7 @@ class AmiSyncTest {
     val keyPrefix = KeyPrefix("ami/")
     val config = new MemoryConfig(bucket)
 
-    config.s3.keys += Key("ami/machine.img")
+    config.s3.keys += Key("ami/machine/machine-19700101.img")
 
     AmiSync.run(config, bucket, keyPrefix, skipDelays = true)
 
@@ -21,7 +21,7 @@ class AmiSyncTest {
         snapshots = Map(
           SnapshotId("snap-002") -> MemorySnapshot(
             SnapshotId("snap-002"),
-            description = "machine",
+            description = "machine-19700101",
             encrypted = true,
             progress = 100
           )
@@ -29,8 +29,8 @@ class AmiSyncTest {
         amis = Map(
           AmiId("ami-001") -> MemoryAmi(
             AmiId("ami-001"),
-            name = AmiName("machine"),
-            description = "machine",
+            name = AmiName("machine-19700101"),
+            description = "machine-19700101",
             snapshotId = SnapshotId("snap-002")
           )
         )
@@ -38,7 +38,7 @@ class AmiSyncTest {
       config.ec2
     )
 
-    config.s3.keys -= Key("ami/machine.img")
+    config.s3.keys -= Key("ami/machine/machine-19700101.img")
 
     AmiSync.run(config, bucket, keyPrefix, skipDelays = true)
 
