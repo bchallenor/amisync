5b3ee339a899b6e76f9d8317f3ff61352f9e85f0
Ben Challenor <ben@challenor.org>
2018-10-12 23:30:42 +0100

    Test that delimiter is not set

--- src/test/scala/amisync/memory/MemoryS3.scala
+++ src/test/scala/amisync/memory/MemoryS3.scala
@@ -2,16 +2,18 @@ package amisync.memory
 
 import amisync._
 import com.amazonaws.services.s3.AbstractAmazonS3
-import com.amazonaws.services.s3.model.{ObjectListing, S3ObjectSummary}
+import com.amazonaws.services.s3.model.{ListObjectsRequest, ObjectListing, S3ObjectSummary}
 
 case class MemoryS3(
   bucket: Bucket,
   var keys: Set[Key] = Set.empty
 ) extends AbstractAmazonS3 {
-  override def listObjects(bucketName: String, prefix: String): ObjectListing = {
-    require(bucketName == bucket.name)
+  override def listObjects(req: ListObjectsRequest): ObjectListing = {
+    require(req.getBucketName == bucket.name)
+    require(req.getDelimiter eq null)
+
     val listing = new ObjectListing
-    keys.iterator.filter(_.name.startsWith(prefix)).foreach { key =>
+    keys.iterator.filter(_.name.startsWith(req.getPrefix)).foreach { key =>
       val summary = new S3ObjectSummary
       summary.setKey(key.name)
       listing.getObjectSummaries.add(summary)
